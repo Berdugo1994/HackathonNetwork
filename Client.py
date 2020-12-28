f8fd459557ab8c73eff5c5eae4eec90d31cb1ac7
Eden Berdugo <62346207+Berdugo1994@users.noreply.github.com>
2020-12-28 16:04:52 +0200

    StartedFromLocal

---
+++ Client.py
@@ -0,0 +1,17 @@
+from socket import *
+from getch import _Getch
+
+serverName = 'localhost'
+serverPort = 12000
+TeamName = b'EdenAndSarit\n'
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+while (True):
+    # getch = _Getch()
+    # clientSocket.send(getch)
+    #input_char = msvcrt.getch()
+    clientSocket.send(TeamName)
+    # modifiedSenetence = clientSocket.recv(1024)
+    if TeamName == '`':  # For Testing
+        clientSocket.close()
+        break
