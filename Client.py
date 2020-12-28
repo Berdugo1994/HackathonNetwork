7ff09ba73b7aaf2afe522f37bcc6b26026fe3ebf
berdugo1994 <berdugogo@gmail.com>
2020-12-28 23:22:50 +0200

    last commit night

--- Client.py
+++ Client.py
@@ -1,18 +1,35 @@
 from socket import *
-from getch import _Getch
-#TESTSSH
-hello="SSSSS"
-serverName = 'localhost'
-serverPort = 12000
-TeamName = b'EdenAndSarit\n'
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-while (True):
-    # getch = _Getch()
-    # clientSocket.send(getch)
-    #input_char = msvcrt.getch()
-    clientSocket.send(TeamName)
-    # modifiedSenetence = clientSocket.recv(1024)
-    if TeamName == '`':  # For Testing
-        clientSocket.close()
-        break
+from scapy.all import get_if_addr
+import struct
+def listening_to_server_udp():
+    UDPclientSocket = socket(AF_INET, SOCK_DGRAM)
+    UDPclientSocket.setsockopt(SOL_SOCKET,SO_BROADCAST,1)
+    #UDPclientSocket.setsockopt(SOL_SOCKET,SO_REUSEPORT,1)
+    UDPclientSocket.bind((serverName,serverPort))
+    print("Client started, listening for offer requests...​")
+    while (True):
+        server_msg, serverAdress = UDPclientSocket.recvfrom(2048) #Wait for message from udp
+        print("Recieved offer from " + str(serverAdress) + ", attempting to connect...")
+        server_port = struct.unpack('0xfeedbeef', server_msg)
+        print(server_port)
+
+def client_tcp_network_with_server(cur_server_ip, cur_server_port):
+    client_socket = socket(AF_INET,SOCK_STREAM)
+    client_socket.connect((cur_server_ip, cur_server_port))
+    client_socket.send(b'client says -> tcp connection succeed')
+
+
+
+
+
+
+
+### CLIENT UDP
+if __name__=='__main__':
+    #localIP = get_if_addr('eth1')
+    # serverName = '0.0.0.0'
+    serverName= 'localhost'
+    serverPort = 13117
+    TeamName = b'EdenAndSarit\n'
+    print("TEST")
+    listening_to_server_udp()
--- Server.py
+++ Server.py
@@ -1,11 +1,47 @@
-from socket import *
-serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
-print('THE SERVER IS RDY TO RECIEVE!')
-while True:
-    connectionSocket, addr =serverSocket.accept()
-    sentence = connectionSocket.recv(1024)
-    print("Team " + sentence + "was added to the party, welcome ")
-    connectionSocket.send(b"welcome")
+from scapy.all import get_if_addr
+import socket
+import time
+import struct
+from _thread import *
+import threading
+def server_connection_UDP():
+    udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    udp_server_socket.bind((localIP, server_static_port))
+    print("​Server started, listening on IP address" + str(localIP))
+    message = struct.pack('Ibh', 0xfeedbeef, 0x2, UDP_port)
+    count = 0
+    while count < 1000:
+        time.sleep(1)
+        count = 0
+        while count < 100:
+            udp_server_socket.sendto(message, ('255.255.255.255', UDP_port))
+            time.sleep(1)
+            establish_tcp_connection()
+            count = count + 1
+
+
+def server_tcp():
+    tcp_server_master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_server_master_socket.bind((localIP, TCP_server_master_port))
+    tcp_server_master_socket.listen(1)
+    return tcp_server_master_socket
+
+
+def establish_tcp_connection():
+    connection_socket, client_address = tcp_master_socket.accept()
+    sockets_list.append(connection_socket)
+    msg_from_client = connection_socket.recv(1024)
+
+
+if __name__ == '__main__':
+    #localIP = get_if_addr('eth1')
+    localIP = 'localhost'
+    UDP_port = 13117
+    bufferSize = 1024
+    server_static_port = 2057
+    TCP_server_master_port = 2058
+    tcp_master_socket = server_tcp()
+    server_connection_UDP()
+    sockets_list = []
