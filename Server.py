6ba55ac7f169377cd6d2bcfeadb023e96f970da1
EdenBerdugo <berdugoe@post.bgu.ac.il>
2020-12-28 17:08:48 +0200

    check

--- Server.py
+++ Server.py
@@ -3,7 +3,7 @@ serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
-print('THE SERVER IS RDY TO RECIEVE')
+print('THE SERVER IS RDY TO RECIEVE!')
 while True:
     connectionSocket, addr =serverSocket.accept()
     sentence = connectionSocket.recv(1024)
