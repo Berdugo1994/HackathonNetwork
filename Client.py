db9658f94be5a72af600532b63a38cef3cd3d153
EdenBerdugo <berdugoe@post.bgu.ac.il>
2020-12-29 17:42:03 +0200

    asddsa

--- Client.py
+++ Client.py
@@ -1,46 +1,32 @@
-# from socket import *
-# from getch import _Getch
-# from scapy.all import get_if_addr
-# #TESTSSH
-# ip = get_if_addr('eth1')
-# print(ip)
-# serverIpOfBroadcast = '0.0.0.0'
-# serverPort = 13117
-# TeamName = b'EdenAndSarit\n'
-# clientSocket = socket(AF_INET, SOCK_DGRAM)
-# # clientSocket.connect((serverIpOfBroadcast, serverPort))
-# while (True):
-#     # getch = _Getch()
-#     # clientSocket.send(getch)
-#     #input_char = msvcrt.getch()
-#     # clientSocket.send(TeamName)
-#     modifiedSenetence = clientSocket.recv(1024)
-#     print(modifiedSenetence)
-#     if TeamName == '`':  # For Testing
-#         clientSocket.close()
-#         break
-
 from socket import *
 from scapy.all import get_if_addr
+import struct
+
+
+def listening_to_server_udp():
+    UDPclientSocket = socket(AF_INET, SOCK_DGRAM)
+    UDPclientSocket.setsockopt(SOL_SOCKET,SO_BROADCAST,1)
+    UDPclientSocket.setsockopt(SOL_SOCKET,SO_REUSEPORT,1)
+    UDPclientSocket.bind((serverName,serverPort))
+    print("Client started, listening for offer requests...​")
+    while (True):
+        # UDPclientSocket.sendto(message,(serverName,serverPort))
+        server_msg, serverAdress = UDPclientSocket.recvfrom(2048) #Wait for message from udp
+        print("Recieved offer from " + str(serverAdress) + ", attempting to connect...")
+        server_port = struct.unpack('Ibh', server_msg)
+        if len(server_port)>6:
+            continue
+        # modifiedSenetence = clientSocket.recv(1024)
+        # if TeamName == '`':  # For Testing
+        #     clientSocket.close()
+        #     break
+
+
 ### CLIENT UDP 
-localIP = get_if_addr('eth1')
-print(localIP)
-serverName = '0.0.0.0'
-serverPort = 13117
-TeamName = b'EdenAndSarit\n'
-UDPclientSocket = socket(AF_INET, SOCK_DGRAM)
-UDPclientSocket.setsockopt(SOL_SOCKET,SO_BROADCAST,1)
-UDPclientSocket.setsockopt(SOL_SOCKET,SO_REUSEPORT,1)
-UDPclientSocket.bind((serverName,serverPort))
-print("WAS HERE")
-# UDPclientSocket.bind((serverName,serverPort))
-message=b"TTTTTTTTesTTTTTTTTTTTT"
-while (True):
-    # UDPclientSocket.sendto(message,(serverName,serverPort))
-    modifiedSenetence, serverAdress = UDPclientSocket.recvfrom(2048) #Wait for message from udp
-    print(modifiedSenetence)
-    print(serverAdress)
-    # modifiedSenetence = clientSocket.recv(1024)
-    # if TeamName == '`':  # For Testing
-    #     clientSocket.close()
-    #     break
+if __name__=='__main__':
+    localIP = get_if_addr('eth1')
+    serverName = '0.0.0.0'
+    serverPort = 13117
+    TeamName = b'EdenAndSarit\n'
+    print("TEST")
+    listening_to_server_udp()
--- Server.py
+++ Server.py
@@ -1,5 +1,7 @@
 from scapy.all import get_if_addr
 import socket
+import time
+import struct
 localIP = get_if_addr('eth1')
 localPort = 13117
 bufferSize = 1024
@@ -12,14 +14,19 @@ UDPServerSocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT,1)
 # UDPServerSocket.setsockopt(socket.SOL_SOCKET,socket.INADDR_BROADCAST,1)
 # Bind to address and ip
 UDPServerSocket.bind((localIP, 2057))
-print("UDP server up and listening")
-message = b"your very important message"
+print("​Server started, listening on IP address" + str(localIP)
+#print(string_message)
+#message = str.encode(string_message)
 # Listen for incoming datagrams
+
+struct.pack('0xfeedbeef','0x2',localPort)
 count =0
 while(count<1000):
+    time.sleep(1)
     count=0
     while(count<100):
         UDPServerSocket.sendto(message, ('255.255.255.255', localPort))
+        time.sleep()
         # bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
         # message = bytesAddressPair[0]
         # address = bytesAddressPair[1]
